c5294c088f2725982f49321a1e20dac9215ec8a8
Eilene <eileneftf@gmail.com>
2026-03-16 18:59:29 -0400

    import fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='hrr',
-        version='0.0.3',
+        version='0.0.4',
         description='Defines a class/object for Holographic Reduced Representations',
         author='Mary Kelly',
         author_email='marykelly4@carleton.ca',
